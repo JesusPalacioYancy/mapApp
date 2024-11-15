8cc29f6c88eb7bab0698054039e19d0d22d4a9fe
Jesus Palacio Yancy <jpalacio@transportationamerica.com>
2024-11-15 15:07:00 -0500

    Inicio seccion 21 | cap 297

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -17,7 +17,7 @@ export class FullScreenPageComponent implements AfterViewInit{
     const map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: [-74.79367470295588, 10.919224141582049], // starting position [lng, lat]
       zoom: 9, // starting zoom
     });
   };
--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrl: './markers-page.component.css'
 })
-export class MarkersPageComponent {
 
-}
+export class MarkersPageComponent {
+  // propiedad del mapa '?'
+  public map?: Map;
+
+  // propiedad de LNG y LAT
+  public lngLat: LngLat = new LngLat(-74.79367470295588, 10.919224141582049);
+
+
+  // Obtener valor de la etiqueta
+  @ViewChild('map') divMap?: ElementRef;
+
+  // Creacion del mapa
+  ngAfterViewInit(): void {
+  if( !this.divMap ) throw 'El elemento html no fue encontrado'
+  
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+    });
+
+    // Ejemplo de marcador
+    // const markerHtml = document.createElement('div')
+    // markerHtml.innerHTML = 'JP'
+    
+    // const markers = new Marker({
+    //   color: 'red',
+    //   element: markerHtml
+    // })
+    //   .setLngLat(this.lngLat)
+    //   .addTo(this.map)
+  };
+
+  // crear marca
+  createMarker() {
+    if(!this.map) return;
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const lgnLat = this.map.getCenter();
+
+    this.addMarker(lgnLat, color)
+  };
+
+
+  // añadir marca
+  addMarker(lngLat: LngLat, color: string ) {
+    if(!this.map) return;
+
+    const marker = new Marker({
+      color: color,
+      draggable: true
+    })
+      .setLngLat(lngLat)
+      .addTo(this.map)
+  };
+
+
+ 
+};
