c29ee70b8886fa761419ae2e5e4dc23d93e8a0bf
Jesus Palacio Yancy <jpalacio@transportationamerica.com>
2024-11-18 15:41:38 -0500

    Inicio y fin de seccion 22

--- src/app/alone/components/side-menu/side-menu.component.ts
+++ src/app/alone/components/side-menu/side-menu.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 interface menuItem {
   route: string;
   name: string;
-}
+};
 
 @Component({
-  selector: 'maps-side-menu',
+  selector: 'side-menu',
+  standalone: true,
+  imports: [CommonModule, RouterModule ],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
@@ -17,6 +21,7 @@ export class SideMenuComponent {
     {route: '/maps/zoomrange', name: 'ZoomRange'},
     {route: '/maps/markers', name: 'Markers'},
     {route: '/maps/properties', name: 'Houses'},
-  ]
+    {route: '/alone', name: 'AlonePage'},
+  ];
 
-}
+};
